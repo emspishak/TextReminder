ed50e9464c60df89306d7427c7764d4047731845
Eric Spishak <emspishak@gmail.com>
2012-08-08 22:51:16 -0700

    Get count of unread texts.

--- src/com/hotcats/textreminder/AlarmReceiver.java
+++ src/com/hotcats/textreminder/AlarmReceiver.java
@@ -3,6 +3,8 @@ package com.hotcats.textreminder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.util.Log;
 
 /**
@@ -10,8 +12,15 @@ import android.util.Log;
  */
 public class AlarmReceiver extends BroadcastReceiver {
 
+    public static final Uri SMS_INBOX = Uri.parse("content://sms/inbox");
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i("alarm", "alarm ring received");
+
+        Cursor c = context.getContentResolver().query(SMS_INBOX, null, "read = 0", null, null);
+        int unread = c.getCount();
+        c.close();
+        Log.i("alarm", "found " + unread + " unread texts");
     }
 }
