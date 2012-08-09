cbe5463e71a742b342d41d1a04c7c47f77aa9ffe
Eric Spishak <emspishak@gmail.com>
2012-08-08 23:10:19 -0700

    Clear alarm if there are no unread messages left.

--- src/com/hotcats/textreminder/AlarmReceiver.java
+++ src/com/hotcats/textreminder/AlarmReceiver.java
@@ -1,5 +1,7 @@
 package com.hotcats.textreminder;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -22,5 +24,12 @@ public class AlarmReceiver extends BroadcastReceiver {
         int unread = c.getCount();
         c.close();
         Log.i("alarm", "found " + unread + " unread texts");
+        if (unread == 0) {
+            AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            Intent i = new Intent(TextReceiver.ALARM_RING);
+            PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
+            am.cancel(pi);
+            Log.i("alarm", "cancelled alarm");
+        }
     }
 }
