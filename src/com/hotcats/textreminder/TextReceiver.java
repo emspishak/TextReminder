e91e0420867e3075b17eed7c7793b9063f7c270c
Eric Spishak <emspishak@gmail.com>
2012-08-09 00:21:50 -0700

    Refactor: move receivers to one file to eliminate some redundancy.

--- src/com/hotcats/textreminder/TextReceiver.java
+++ src/com/hotcats/textreminder/TextReceiver.java
@@ -5,27 +5,66 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Vibrator;
 import android.util.Log;
 
 /**
- * A BroadcastReceiver that gets called when a text message is received.
+ * Receives text messages and alarm rings to alert the user if there are unread texts.
  */
 public class TextReceiver extends BroadcastReceiver {
 
 	public static final int SECOND = 3000;
 	public static final int MINUTE = 1000 * 60;
-	public static final String ALARM_RING = "com.hotcats.textreminder.TextReceiver.ALARM_RING";
 
-	/**
-	 * Receive the text message and set an alarm.
-	 */
+	public static final String ALARM_RING = "com.hotcats.textreminder.TextReceiver.ALARM_RING";
+	public static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+
+	public static final Uri SMS_INBOX = Uri.parse("content://sms/inbox");
+
+    public static final long[] VIBRATE_PATTERN = {0, 250, 250, 250};
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("text", "text message recieved!");
-
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(ALARM_RING);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
+
+        if (ALARM_RING.equals(intent.getAction())) {
+            handleAlarm(context, am, pi);
+        } else if (SMS_RECEIVED.equals(intent.getAction())) {
+            handleText(am, pi);
+        } else {
+            Log.w("all", "invalid intent received: " + intent.getAction());
+        }
+    }
+
+    /**
+     * Handle an alarm ring: if there are unread texts alert the user, otherwise cancel the alarm.
+     */
+    private void handleAlarm(Context context, AlarmManager am, PendingIntent pi) {
+        Log.i("alarm", "alarm ring received");
+
+        Cursor c = context.getContentResolver().query(SMS_INBOX, null, "read = 0", null, null);
+        int unread = c.getCount();
+        c.close();
+        Log.i("alarm", "found " + unread + " unread texts");
+        if (unread == 0) {
+            am.cancel(pi);
+            Log.i("alarm", "cancelled alarm");
+        } else {
+            Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(VIBRATE_PATTERN, -1);
+        }
+    }
+
+    /**
+     * Handle receiving a text: set an alarm to alert the user.
+     */
+    private void handleText(AlarmManager am, PendingIntent pi) {
+        Log.i("text", "text message recieved!");
+
         am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + SECOND, SECOND, pi);
     }
 }
