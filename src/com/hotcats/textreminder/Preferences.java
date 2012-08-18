4179c98bd1bdd945ef111aa2135b9d8a44c654a2
Eric Spishak <emspishak@gmail.com>
2012-08-17 19:32:28 -0700

    Update the repeat delay preference to show the currently selected value.

--- src/com/hotcats/textreminder/Preferences.java
+++ src/com/hotcats/textreminder/Preferences.java
@@ -1,16 +1,52 @@
 package com.hotcats.textreminder;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.util.Log;
 
 /**
  * Handles preferences.
  */
-public class Preferences extends PreferenceActivity {
+public class Preferences extends PreferenceActivity implements
+        OnSharedPreferenceChangeListener {
+
+    public static final String PREF_REPEAT_DELAY = "pref_repeatDelay";
+
+    private ListPreference repeatDelayPreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(this);
+
+        PreferenceScreen prefs = getPreferenceScreen();
+        repeatDelayPreference = (ListPreference) prefs
+                .findPreference(PREF_REPEAT_DELAY);
+        updateRepeatDelaySummary();
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+            String key) {
+        String value = sharedPreferences.getString(key, "-1");
+        Log.i("preferences", "preference " + key + " changed to " + value);
+        if (PREF_REPEAT_DELAY.equals(key)) {
+            updateRepeatDelaySummary();
+        }
+    }
+
+    /**
+     * Sets the summary of the repeatDelay preference to be the value of the
+     * currently selected entry.
+     */
+    private void updateRepeatDelaySummary() {
+        repeatDelayPreference.setSummary(repeatDelayPreference.getEntry());
     }
 }
